005854f18aed3ddd5f4eed8f7135730f62ec55c7
ayaangazali <ayaangazali.work@gmail.com>
2018-02-07 08:34:58 -0800

    add products.js with spinner array

---
+++ products.js
@@ -0,0 +1,7 @@
+// list of all the spinners we sell
+var products = [
+  { id: 1, name: "Classic Tri-Spinner", price: 9.99, image: "images/spinner1.png" },
+  { id: 2, name: "Rainbow Metal Spinner", price: 14.99, image: "images/spinner2.png" },
+  { id: 3, name: "Glow in the Dark Spinner", price: 12.50, image: "images/spinner3.png" },
+  { id: 4, name: "Gold Premium Spinner", price: 19.99, image: "images/spinner4.png" }
+];
