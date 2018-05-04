8f688aa245adf2792b4d0bd1d851d979ade364e0
ayaangazali <ayaangazali.work@gmail.com>
2018-05-04 11:47:02 -0700

    add more spinners to product list

--- products.js
+++ products.js
@@ -3,5 +3,9 @@ var products = [
   { id: 1, name: "Classic Tri-Spinner", price: 9.99, image: "images/spinner1.png" },
   { id: 2, name: "Rainbow Metal Spinner", price: 14.99, image: "images/spinner2.png" },
   { id: 3, name: "Glow in the Dark Spinner", price: 12.50, image: "images/spinner3.png" },
-  { id: 4, name: "Gold Premium Spinner", price: 19.99, image: "images/spinner4.png" }
+  { id: 4, name: "Gold Premium Spinner", price: 19.99, image: "images/spinner4.png" },
+  { id: 5, name: "Black Ninja Spinner", price: 11.99, image: "images/spinner5.png" },
+  { id: 6, name: "Camo Army Spinner", price: 13.99, image: "images/spinner6.png" },
+  { id: 7, name: "LED Light Up Spinner", price: 16.99, image: "images/spinner7.png" },
+  { id: 8, name: "Mini Keychain Spinner", price: 7.99, image: "images/spinner8.png" }
 ];
